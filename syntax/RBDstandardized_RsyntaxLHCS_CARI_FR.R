80dffa88aa73f342b5acbd37339c94ffd94247ef
olanderb <billyolander@gmail.com>
2021-02-22 15:38:15 +0000

    more more

--- syntax/RBDstandardized_RsyntaxLHCS_CARI_FR.R
+++ syntax/RBDstandardized_RsyntaxLHCS_CARI_FR.R
@@ -13,19 +13,19 @@ dataset <- dataset %>% mutate(stress_coping = case_when(
   LhCSIStress2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSIStress3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSIStress4 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
-  TRUE ~ "No"))
+  TRUE ~ "Non"))
 #Crisis
 dataset <- dataset %>% mutate(crisis_coping = case_when(
   LhCSICrisis1 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSICrisis2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSICrisis3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
-  TRUE ~ "No"))
+  TRUE ~ "Non"))
 #Emergency
 dataset <- dataset %>% mutate(emergency_coping = case_when(
   LhCSIEmergency1 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSIEmergency2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
   LhCSIEmergency3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas continuer à le faire") ~ "Oui",
-  TRUE ~ "No"))
+  TRUE ~ "Non"))
 #calculate Max_coping_behaviour
 dataset <- dataset %>% mutate(LhCSICat = case_when(
   emergency_coping == "Oui" ~ "StrategiesdeUrgence",
